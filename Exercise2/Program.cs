017d39f89ba7d1e71d71af726107af0355c26cc2
Ivan <petiv12@yandex.ru>
2022-11-13 20:59:06 +0300

    добавили решение задания №2

---
+++ Exercise2/Program.cs
@@ -0,0 +1,23 @@
+﻿int[] numbers = new int[5];
+
+for(int i = 0; i < numbers.Length; i++)
+{
+numbers[i] = new Random().Next(-100,100);
+}
+   
+for(int i = 0; i < numbers.Length; i++)
+{
+Console.Write(numbers[i] + ",");
+}
+Console.WriteLine();
+
+int count = 0;
+for(int j = 0; j < numbers.Length; j++)
+{
+    for (int i = 0; i < 3; i++)
+    {
+        Console.Write(numbers[i] + ",");
+    }
+    Console.WriteLine();
+}
+
