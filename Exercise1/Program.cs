4559f215b78989cbed3fb1eb88e3b81a8598d95e
Ivan <petiv12@yandex.ru>
2022-11-13 20:52:48 +0300

    добавили код решения задачи

---
+++ Exercise1/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Enter name:");
+string name = Console.ReadLine();
+
+if(name.ToLower() == "ivan")
+{
+    Console.WriteLine("Да здравствует Иван!!!");
+}
+else
+{
+    Console.Write("Hello, ");
+    Console.Write(name);
+}
