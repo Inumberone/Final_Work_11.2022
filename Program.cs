902438369d29212b40e7dadf60e3573adf3a7d33
Петров Иван <112970292+Inumberone@users.noreply.github.com>
2022-11-14 23:35:17 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿// Написать программу, которая из имеющегося массива строк, формирует массив из строк, длина которых меньше, либо равна 3
+
+
+
+string[] array = new string[] {"line", "rows", "columns", "int", "log", "graph", "dot", "net"};
+string[] array1 = new string[array.Length];
+
+GetArray(array, array1);
+PrintArray(array);
+Console.WriteLine();
+PrintArray(array1);
+Console.WriteLine();
+
+void GetArray(string[] array, string[] array1)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(array[i].Length <= 3)
+        {
+            array1[count] = array[i];
+            count++;
+        }
+    }
+}
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }    
+}
